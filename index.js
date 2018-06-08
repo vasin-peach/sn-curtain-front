9fcf03688bf63f90e5c136334188b9f687355523
Peaches <shinasaki07@gmail.com>
2018-06-08 22:19:26 +0700

    :bug: change port 5000 to 3000

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging')
   app.use(express.static('frontend/dist'))
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT)

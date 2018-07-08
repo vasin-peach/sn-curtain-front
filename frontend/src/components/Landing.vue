f31681c64e639ab37f071a86c898fc97350798df
Peaches <shinasaki07@gmail.com>
2018-07-08 16:17:21 +0700

    :iphone: init golden ratio.

--- frontend/src/components/Landing.vue
+++ frontend/src/components/Landing.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="landing">
-    Landing
+    <div class="box w-xl h-xl">1</div>
+    <div class="box w-lg h-lg">2</div>
+    <div class="box w-md h-md">3</div>
+    <div class="box w-sm h-sm">4</div>
+    <div class="box w-xs h-xs">5</div>
   </div>
 </template>
 

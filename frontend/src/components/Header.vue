fc3db8fd1ef804748af2e41b241fafed242b6713
Peaches <shinasaki07@gmail.com>
2018-07-13 19:32:01 +0700

    :iphone: change burger menu to right and menu bar progressive now.

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -2,15 +2,15 @@
   <div class="header">
     <div class="header-container bg-dark1 h-sm">
       <div class="row">
-        <div class="col-6 col-md-4 menu">
-          <input type="checkbox" id="menu-toggle">
-          <label id="trigger" for="menu-toggle"></label>
-          <label id="burger" for="menu-toggle"></label>
+        <div class="col-0 col-md-4">
         </div>
         <div class="col-6 col-md-4 brand">
           <img src="@image/brand.svg">
         </div>
-        <div class="col col-md-4">
+        <div class="col-6 col-md-4 menu">
+          <input type="checkbox" id="menu-toggle">
+          <label id="trigger" for="menu-toggle"></label>
+          <label id="burger" for="menu-toggle"></label>
         </div>
       </div>
     </div>

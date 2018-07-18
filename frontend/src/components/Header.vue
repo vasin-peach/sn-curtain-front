07aa7fe177c996487dda275381b573dcbcbf9a29
Peaches <shinasaki07@gmail.com>
2018-07-18 15:51:14 +0700

    :iphone: solve user spam click burger menu.

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -9,7 +9,7 @@
             <img src="@image/brand.svg">
           </div>
           <div class="col-6 col-md-4 menu">
-            <input type="checkbox" id="menu-toggle" v-model="menu_toggle">
+            <input type="checkbox" id="menu-toggle" v-model="menu_toggle" :disabled="menu_state">
             <label id="trigger" for="menu-toggle"></label>
             <label id="burger" for="menu-toggle"></label>
           </div>
@@ -43,42 +43,54 @@ export default {
   name: 'Header',
   data() {
     return {
-      menu_toggle: false
+      menu_toggle: false,
+      menu_state: false,
     }
   },
   watch: {
     menu_toggle: function(status) {
-
-      var headerMenu = $('.header-menu');
-      var headerFade = $('.header-fade');
+      var _this = this;
 
       // Menu On
       if (status) {
-        headerMenu.velocity("finish", true).velocity("slideDown", { duration: 300 }, [.53,.49,.18,.97]);
-        headerFade.velocity("finish", true).velocity("slideDown", { duration: 300 }, [.53,.49,.18,.97]);
+        $('.header-menu').velocity("slideDown", { duration: 300 }, [.53,.49,.18,.97]);
+        $('.header-fade').velocity("slideDown", { duration: 300 }, [.53,.49,.18,.97]);
         // text animate
-        $('#menu-1').velocity("finish", true).velocity({ opacity: "1", top: "40px" }, { duration: 300, delay: 50, _cacheValues:false}, [.53,.49,.18,.97]);
-        $('#menu-2').velocity("finish", true).velocity({ opacity: "1", top: "40px" }, { duration: 350, delay: 100, _cacheValues:false}, [.53,.49,.18,.97]);
-        $('#menu-3').velocity("finish", true).velocity({ opacity: "1", top: "40px" }, { duration: 400, delay: 150, _cacheValues:false}, [.53,.49,.18,.97]);
-        $('#menu-4').velocity("finish", true).velocity({ opacity: "1", top: "40px" }, { duration: 450, delay: 200, _cacheValues:false}, [.53,.49,.18,.97]);
+        $('#menu-1').velocity({ opacity: "1", top: "40px" }, { duration: 300, delay: 50, _cacheValues:false}, [.53,.49,.18,.97]);
+        $('#menu-2').velocity({ opacity: "1", top: "40px" }, { duration: 350, delay: 100, _cacheValues:false}, [.53,.49,.18,.97]);
+        $('#menu-3').velocity({ opacity: "1", top: "40px" }, { duration: 400, delay: 150, _cacheValues:false}, [.53,.49,.18,.97]);
+        $('#menu-4').velocity({ opacity: "1", top: "40px" }, { duration: 450, delay: 200, _cacheValues:false}, [.53,.49,.18,.97]);
         
         // disable scroll
         $('html').addClass('stop-scrolling');
         $('html').bind('touchmove', function(e) {e.preventDefault()});
 
+        // disable button
+        this.menu_state = true;
+        setTimeout(function () {
+          _this.menu_state = false;
+        }, 450);
+        
+
       // Menu Off
       } else {
-        headerMenu.velocity("finish", true).velocity("slideUp", { duration: 300, delay: 150 }, [.53,.49,.18,.97]);
-        headerFade.velocity("finish", true).velocity("slideUp", { duration: 300, delay: 250 }, [.53,.49,.18,.97]);
+        $('.header-menu').velocity("slideUp", { duration: 300, delay: 150 }, [.53,.49,.18,.97]);
+        $('.header-fade').velocity("slideUp", { duration: 300, delay: 250 }, [.53,.49,.18,.97]);
         // text animate
-        $('#menu-1').velocity("finish", true).velocity({ opacity: "0", top: "0px" }, { duration: 150}, [.53,.49,.18,.97]);
-        $('#menu-2').velocity("finish", true).velocity({ opacity: "0", top: "0px" }, { duration: 200}, [.53,.49,.18,.97]);
-        $('#menu-3').velocity("finish", true).velocity({ opacity: "0", top: "0px" }, { duration: 250}, [.53,.49,.18,.97]);
-        $('#menu-4').velocity("finish", true).velocity({ opacity: "0", top: "0px" }, { duration: 300}, [.53,.49,.18,.97]);
+        $('#menu-1').velocity({ opacity: "0", top: "0px" }, { duration: 150}, [.53,.49,.18,.97]);
+        $('#menu-2').velocity({ opacity: "0", top: "0px" }, { duration: 200}, [.53,.49,.18,.97]);
+        $('#menu-3').velocity({ opacity: "0", top: "0px" }, { duration: 250}, [.53,.49,.18,.97]);
+        $('#menu-4').velocity({ opacity: "0", top: "0px" }, { duration: 300}, [.53,.49,.18,.97]);
 
         // enable scroll
         $('html').removeClass('stop-scrolling');
         $('html').unbind('touchmove');
+
+        // disable button
+        this.menu_state = true;
+        setTimeout(function () {
+          _this.menu_state = false;
+        }, 450);
       }
     }
   }

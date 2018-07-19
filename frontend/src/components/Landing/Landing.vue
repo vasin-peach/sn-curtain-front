401ab278e328e00657525f729e42e70d1cb101db
Peaches <shinasaki07@gmail.com>
2018-07-19 21:22:47 +0700

    :bug:|:iphone: fix box-shadow not show in section2.

--- frontend/src/components/Landing/Landing.vue
+++ frontend/src/components/Landing/Landing.vue
@@ -3,6 +3,7 @@
     <section1></section1>
     <section2></section2>
     <section3></section3>
+    <section4></section4>
   </div>
 </template>
 
@@ -10,13 +11,15 @@
 import Section1 from './Section1';
 import Section2 from './Section2';
 import Section3 from './Section3';
+import Section4 from './Section4';
 export default {
   name: 'Landing',
 
   components: {
     Section1,
     Section2,
-    Section3
+    Section3,
+    Section4,
   }
 }
 </script>
---
+++ frontend/src/components/Landing/Section4.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="section4">
+    <div class="container">
+      Section4 Ja
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Section4',
+}
+</script>
+
+<style>
+</style>

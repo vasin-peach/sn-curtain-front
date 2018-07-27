d186c745d72d1e165d3c4470bd4189c1e8655973
Peaches <shinasaki07@gmail.com>
2018-07-28 06:27:25 +0700

    :art: add gzip in font.

--- frontend/build/webpack.prod.conf.js
+++ frontend/build/webpack.prod.conf.js
@@ -10,6 +10,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 
 const env = process.env.NODE_ENV === 'testing' ?
   require('../config/test.env') :
@@ -55,24 +56,21 @@ const webpackConfig = merge(baseWebpackConfig, {
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
     new OptimizeCSSPlugin({
-      cssProcessorOptions: config.build.productionSourceMap ?
-        {
-          safe: true,
-          map: {
-            inline: false
-          }
-        } :
-        {
-          safe: true
+      cssProcessorOptions: config.build.productionSourceMap ? {
+        safe: true,
+        map: {
+          inline: false
         }
+      } : {
+        safe: true
+      }
     }),
     // generate dist index.html with correct asset hash for caching.
     // you can customize output by editing /index.html
     // see https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: process.env.NODE_ENV === 'testing' ?
-        'index.html' :
-        config.build.index,
+        'index.html' : config.build.index,
       template: 'index.html',
       inject: true,
       minify: {
@@ -130,7 +128,11 @@ const webpackConfig = merge(baseWebpackConfig, {
       jquery: 'jquery',
       'window.jQuery': 'jquery',
       jQuery: 'jquery'
-    })
+    }),
+    new LodashModuleReplacementPlugin({
+      collections: true,
+      paths: true,
+    }),
   ]
 })
 

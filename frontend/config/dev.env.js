b55669f39c655fb6bd0d1b4668feea9e3dce8386
Peaches <shinasaki07@gmail.com>
2018-07-29 06:53:05 +0700

    :bug: test environment.

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -4,4 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
+  BACKEND_URI: 'http://localhost:5000'
 })
--- frontend/config/prod.env.js
+++ frontend/config/prod.env.js
@@ -1,4 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
+  BACKEND_URI: process.env.BACKEND_URI_PROD
 }

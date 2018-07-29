4d141b234655c643f8d6cf31c9840856348ef1e1
Peaches <shinasaki07@gmail.com>
2018-07-29 13:45:13 +0700

    test

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BACKEND_URI: '"http://localhost:5000"'
+  BACKEND_URI: '"http://localhost:5000"',
+  TEST: JSON.stringify(process.env.TEST)
 })

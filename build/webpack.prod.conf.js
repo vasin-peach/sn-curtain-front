a42be23c84d9ab62bdbd09f097873b864d9f2b19
Peaches <shinasaki07@gmail.com>
2018-07-31 22:00:50 +0700

    test

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -35,16 +35,19 @@ const webpackConfig = merge(baseWebpackConfig, {
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
-    new webpack.DefinePlugin({
-      // 'process.env': require('../config/prod.env')
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) || '"production"',
-      'process.env.BACKEND_URI': JSON.stringify(process.env.BACKEND_URI) || '"TEST"',
-      'process.env.TEST': JSON.stringify('Test')
-      // 'process.env': require('../config/prod.env'),
-      // 'process.env': JSON.stringify(process.env.NODE_ENV),
-      // 'BACKEND_URI': process.env.BACKEND_URI
+    // new webpack.DefinePlugin({
+    //   // 'process.env': require('../config/prod.env')
+    //   'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) || '"production"',
+    //   'process.env.BACKEND_URI': JSON.stringify(process.env.BACKEND_URI) || '"TEST"',
+    //   'process.env.TEST': JSON.stringify('Test')
+    //   // 'process.env': require('../config/prod.env'),
+    //   // 'process.env': JSON.stringify(process.env.NODE_ENV),
+    //   // 'BACKEND_URI': process.env.BACKEND_URI
+    // }),
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: process.env.NODE_ENV || 'production',
+      BACKEND_URI: process.env.BACKEND_URI || 'test',
     }),
-    // new webpack.EnvironmentPlugin(['NODE_ENV', 'BACKEND_URI']),
     new UglifyJsPlugin({
       uglifyOptions: {
         compress: {

7d87c7eb3c0e6465be6fa86ff101ac51a2dde7ab
Peaches <shinasaki07@gmail.com>
2018-07-31 23:31:02 +0700

    remove dist

--- dist/static/js/6.7afda7fe4e81d338eaf1.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([6],{"5TjR":function(e,n){},"s6+2":function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement,n=this._self._c||e;return n("div",{attrs:{id:"main"}},[n("router-view")],1)},staticRenderFns:[]};var i=t("VU/8")({name:"Main"},r,!1,function(e){t("5TjR")},null,null);n.default=i.exports}});
-//# sourceMappingURL=6.7afda7fe4e81d338eaf1.js.map
--- dist/static/js/7.70c71f71f444021320ae.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([7],{aaaN:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement;return(this._self._c||e)("div",{staticClass:"service"},[this._v("\n  Service\n")])},staticRenderFns:[]};var i=t("VU/8")({name:"Service"},r,!1,function(e){t("qrqM")},null,null);n.default=i.exports},qrqM:function(e,n){}});
-//# sourceMappingURL=7.70c71f71f444021320ae.js.map

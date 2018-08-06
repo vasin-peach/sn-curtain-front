55935d1b809a7da84611f66c83877d016c45bf40
Peaches <shinasaki07@gmail.com>
2018-08-06 20:43:26 +0700

    :art: finish show product in storeview

--- src/components/Store/StoreView.vue
+++ src/components/Store/StoreView.vue
@@ -1,15 +1,42 @@
 <template>
   <div id="storeview">
-    <transition name="fade" mode="out-in">
-      <div v-if="!storeData() || loadingData().store">
-        <loading></loading>
-      </div>
-      <div class="store-container container" v-else key="content">
-        <div v-for="(item, count) in storeData()" :key="count">
-          {{ item }}
+    <div class="container">
+      <transition name="fade" mode="out-in">
+        <div v-if="!storeData() || loadingData().store">
+          <loading></loading>
         </div>
-      </div>
-    </transition>
+        <div class="row store-container" v-else key="content">
+          <div class="col-6 col-sm-6 col-lg-4 col-xl-3 store-item" v-for="(item, count) in storeData()" :key="count">
+            <router-link :to="{ name: 'Product' }">
+              <div class="card-container">
+                <div class="card-head">
+                  <img v-lazy="item.brand.src" :alt="item.name[0].val || 'ชื่อสินค้า'">
+                </div>
+                <div class="card-content">
+                  <p class="title">{{item.name[0].val || 'ชื่อสินค้า' }}</p>
+                  <p class="price">{{item.price || 'ไม่ระบุ' }} บาท</p>
+                  <hr>
+                  <div class="detail row m-0">
+                    <div class="col-12 col-sm-6">
+                      <span class="fabric-title">ผ้า: </span>
+                      <span class="fabric-content">{{ item.fabric || 'ไม่ระบุ' }}</span>
+                    </div>
+                    <span class="color-orange1 d-none d-sm-block">|</span>
+                    <div class="col-12 col-sm-6">
+                      <span class="color-title">สี:</span>
+                      <span class="color-box" :style="{background: item.category.color.hex || '#ccc'}"></span>
+                    </div>
+                  </div>
+                </div>
+                <div class="card-footer">
+                  <div class="button">Buy</div>
+                </div>
+              </div>
+            </router-link>
+          </div>
+        </div>
+      </transition>
+    </div>
   </div>
 </template>
 

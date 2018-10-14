0363fc5259b599e9b31e5056fc09e3f46b654015
Peaches <shinasaki07@gmail.com>
2018-10-14 22:58:33 +0700

    :construction: update address form.

--- src/components/Payment/Address.vue
+++ src/components/Payment/Address.vue
@@ -1,12 +1,78 @@
 <template>
   <div class="payment_address">
-    Payment Address
+    <form @submit.prevent="validateAddress">
+      <div class="address-body">
+        <div class="row">
+          <div class="col-12 col-sm-12 col-md-6">
+            <div class="input-group">
+              <b-form-group label="ชื่อต้น">
+                <b-form-input :state="!errors.has('first_name')" v-validate="'required|min:4'" name="first_name" v-model="form.first_name" type="text" placeholder="ชื่อต้น"></b-form-input>
+                <b-form-invalid-feedback v-show="errors.has('first_name')">
+                  {{ errors.first('first_name') }}
+                </b-form-invalid-feedback>
+              </b-form-group>
+            </div>
+          </div>
+          <div class="col">
+            <div class="input-group">
+              <b-form-group label="นามสกุล">
+                <b-form-input :state="!errors.has('last_name')" v-validate="'required|min:4'" name="last_name" v-model="form.last_name" type="text" placeholder="ชื่อต้น"></b-form-input>
+                <b-form-invalid-feedback v-show="errors.has('last_name')">
+                  {{ errors.first('last_name') }}
+                </b-form-invalid-feedback>
+              </b-form-group>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <div class="input-group">
+              <b-form-group label="เบอร์โทรศัพท์">
+                <b-form-input :state="!errors.has('tel')" v-validate="{ required: true, min:9, max: 10, numeric: true  }" name="tel" v-model="form.tel" type="text" placeholder="เบอร์โทรศัพท์" maxlength="10"></b-form-input>
+                <b-form-invalid-feedback v-show="errors.has('tel')">
+                  {{ errors.first('tel') }}
+                </b-form-invalid-feedback>
+              </b-form-group>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <div class="input-group">
+              <b-form-group label="รหัสไปรษณีย์">
+                <b-form-input :state="!errors.has('zip')" v-validate="{ required: true, regex: /^(\d{5})?$/ }" name="zip" v-model="form.zip" type="text" placeholder="รหัสไปรษณีย์" maxlength="5"></b-form-input>
+                <b-form-invalid-feedback v-show="errors.has('zip')">
+                  {{ errors.first('zip') }}
+                </b-form-invalid-feedback>
+              </b-form-group>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: "payment_address"
+  name: "payment_address",
+  data() {
+    return {
+        form: {
+          first_name: "",
+          last_name: ""
+        }
+    }
+  },
+  methods: {
+    validateAddress() {
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          console.log('hi');
+        }
+      })
+    }
+  }
 };
 </script>
 

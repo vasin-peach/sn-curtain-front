47ca86a1131bd88413da8f1970a3f841a611a591
Peaches <shinasaki07@gmail.com>
2018-10-19 19:24:16 +0700

    :art: change payment options fade transtion 4s to 2s.

--- src/components/Payment/Address.vue
+++ src/components/Payment/Address.vue
@@ -260,4 +260,13 @@ export default {
 </script>
 
 <style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s ease-out;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>

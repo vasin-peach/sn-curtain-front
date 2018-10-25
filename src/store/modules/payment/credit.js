8e1f3e51e3dad05731d5a2f7aafc48886daf0f54
Peaches <shinasaki07@gmail.com>
2018-10-25 21:10:17 +0700

    :art: add user in description

--- src/store/modules/payment/credit.js
+++ src/store/modules/payment/credit.js
@@ -14,34 +14,30 @@ const mutations = {};
 
 // Actions
 const actions = {
-  creditCreateToken({
-    state,
-    getters
-  }, data) {
+  creditCreateToken({ state, getters }, data) {
     return new Promise((resolve, reject) => {
       Omise.setPublicKey(process.env.OMISE_CLIENT);
 
       const card = {
         name: data.card_name,
-        number: data.card_number.replace(/\s/g, ''),
+        number: data.card_number.replace(/\s/g, ""),
         expiration_month: data.expires_date.split("/")[0],
         expiration_year: "20" + data.expires_date.split("/")[1],
         security_code: data.cvv
       };
 
-
-
-
       Omise.createToken("card", card, (statusCode, response) => {
         if (statusCode == 200) {
+          console.log(getters);
 
           const product = {
+            email: getters.auth.email,
             product: localStorage.basket,
             discount: localStorage.discount,
             delivery: localStorage.delivery,
             card_token: response.id,
             card: response.card
-          }
+          };
 
           let urlRequest = process.env.BACKEND_URI + "/payment/charge";
           Vue.http.post(urlRequest, product).then(
@@ -54,7 +50,6 @@ const actions = {
           );
         }
       });
-
     });
   }
 };

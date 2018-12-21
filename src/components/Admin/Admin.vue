4170694bd107e511a7473fada3acf7698fc05865
Peaches <shinasaki07@gmail.com>
2018-12-21 15:59:04 +0700

    :art: improve admin responsive.

--- src/components/Admin/Admin.vue
+++ src/components/Admin/Admin.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="admin">
     <div class="container-header row m-0">
-      <div class="col-4 col-sm-3 col-md-2 header-profile">
+      <div class="col-2 col-sm-3 col-md-2 header-profile">
         <div class="row m-0">
-          <div class="col-5 profile-image d-sm-block d-none">
+          <div class="col-12 col-sm-5 profile-image">
             <div>
               <img src="https://storage.googleapis.com/sn-curtain-dev-profile/shinasaki07@gmail.com--1545032057180">
             </div>
           </div>
-          <div class="col-12 col-sm-7 profile-desc">
+          <div class="col-12 col-sm-7 profile-desc d-sm-block d-none">
             <div>
               <span>Welcome,</span><br>
               <span>Peachesfewffwefwef</span>
@@ -31,67 +31,67 @@
     </div>
     <div class="container-content">
       <div class="row m-0">
-        <div class="content-menu col-4 col-sm-3 col-md-2">
-          <div class="menu-label col">
+        <div class="content-menu col-2 col-sm-3 col-md-2">
+          <div class="menu-label col d-none d-sm-block">
             ทั่วไป
           </div>
-          <hr>
+          <hr class="d-sm-block d-none">
           <div class="menu-list row m-0">
-            <div class="col-2">
+            <div class="col-12 col-sm-3">
               <font-awesome-icon
                 icon="tachometer-alt"
                 aria-hidden="true"
               />
             </div>
-            <div class="col-10">
+            <div class="col-10 col-sm d-none d-sm-block">
               แดชบอร์ด
             </div>
           </div>
           <div class="menu-list row m-0">
-            <div class="col-2">
+            <div class="col-12 col-sm-3">
               <font-awesome-icon
                 icon="chalkboard"
                 aria-hidden="true"
               />
             </div>
-            <div class="col-10">
+            <div class="col-10 col-sm d-none d-sm-block">
               ไสลด์โชว์
             </div>
           </div>
-          <div class="menu-label col">
+          <div class="menu-label col d-none d-sm-block">
             การขาย
           </div>
           <hr>
           <div class="menu-list row m-0">
-            <div class="col-2">
+            <div class="col-12 col-sm-3">
               <font-awesome-icon
                 icon="shopping-cart"
                 aria-hidden="true"
               />
             </div>
-            <div class="col-10">
+            <div class="col-10 col-sm d-none d-sm-block">
               สินค้า
             </div>
           </div>
           <div class="menu-list row m-0">
-            <div class="col-2">
+            <div class="col-12 col-sm-3">
               <font-awesome-icon
                 icon="barcode"
                 aria-hidden="true"
               />
             </div>
-            <div class="col-10">
+            <div class="col-10 col-sm d-none d-sm-block">
               รหัสส่วนลด
             </div>
           </div>
           <div class="menu-list row m-0">
-            <div class="col-2">
+            <div class="col-12 col-sm-3">
               <font-awesome-icon
                 icon="university"
                 aria-hidden="true"
               />
             </div>
-            <div class="col-10">
+            <div class="col-10 col-sm d-none d-sm-block">
               การโอน
             </div>
           </div>

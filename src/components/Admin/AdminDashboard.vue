2c1df5e1eb1460c8fb16eb4a730a9e9e7af36261
Peaches <shinasaki07@gmail.com>
2019-01-17 13:00:16 +0700

    :art: init adminDashnoard.

--- src/components/Admin/AdminDashboard.vue
+++ src/components/Admin/AdminDashboard.vue
@@ -1,13 +1,83 @@
 <template>
-  <div class="admin-dashboard">
-    Dash board
+  <div
+    class="admin-dashboard"
+    ref="admin_container"
+  >
+
+    <transition
+      name="fade"
+      mode="out-in"
+    >
+
+      <div
+        v-if="loadingState"
+        :style="{height: loadingHeight + 'px'}"
+      >
+        <loading class="card-login"></loading>
+      </div>
+
+      <div
+        class="admin-dashboard-container row m-0"
+        key="admin-dashboard"
+        v-else
+      >
+        <div class="admin-dashboard-left card-container col">
+        </div>
+
+      </div>
+
+    </transition>
+
   </div>
 </template>
 
 <script>
+import Loading from "../Loading";
+import Chart from 'chart.js';
+import { mapActions, mapGetters } from "vuex";
 export default {
-  name: 'dashboard'
-}
+  name: "dashboard",
+  data() {
+    return {
+      loadingState: true,
+      loadingHeight: 0
+    };
+  },
+
+  // !METHODS
+  methods: {
+    ...mapActions(["productAll"]),
+
+    // * [TRIGGER] get product
+    async triggerProductGet() {
+      await this.productAll();
+      this.loadingState = false;
+    }
+  },
+
+  // ! COMPUTED
+  computed: {
+    ...mapGetters(["productAllData"])
+  },
+
+  // ! MOUNTED
+  mounted() {
+    // * Trigger get product
+    this.triggerProductGet();
+
+    // * init loading height
+    this.$nextTick(() => {
+      setTimeout(() => {
+        if (this.$refs.admin_container) {
+          this.loadingHeight = this.$refs.admin_container.clientHeight;
+        }
+      }, 2000);
+    });
+  },
+
+  // ! COMPONENTS
+  components: { Loading }
+};
 </script>
 
 <style>

5050b9e4c2ea6a79e86203554d80cbf3cb578bba
Peaches <shinasaki07@gmail.com>
2019-01-18 14:51:27 +0700

    :art: add service.

--- src/components/Landing/Section3.vue
+++ src/components/Landing/Section3.vue
@@ -2,66 +2,76 @@
   <div class='section3'>
     <div class="slider fade2">
       <swiper :options="swiperOption">
-        <swiper-slide v-for="(slide, index) in swiperSlides" :key="index" style="display: block">
+        <swiper-slide
+          v-for="(slide, index) in swiperSlides"
+          :key="index"
+          style="display: block"
+        >
           <router-link :to="{ name: 'Service' }">
-            <img v-lazy="'/static/images/service/service_0' + slide + '.jpg'" :alt="slide">
+            <img
+              v-lazy="'/static/images/service/service_0' + slide + '.jpg'"
+              :alt="slide"
+            >
           </router-link>
         </swiper-slide>
-        <div class="swiper-pagination" slot="pagination"></div>
+        <!-- <div class="swiper-pagination" slot="pagination"></div>
         <div class="swiper-button-prev swiper-button-white" slot="button-prev"></div>
-        <div class="swiper-button-next swiper-button-white" slot="button-next"></div>
+        <div class="swiper-button-next swiper-button-white" slot="button-next"></div> -->
       </swiper>
     </div>
-    <div class="bg-dark1" style="height: 20px"></div>
+    <div
+      class="bg-dark1"
+      style="height: 20px"
+    ></div>
   </div>
 </template>
 
 <script>
-import ScrollMagic from 'scrollmagic';
-import { swiper, swiperSlide } from 'vue-awesome-swiper'
-import 'swiper/dist/css/swiper.css'
+import ScrollMagic from "scrollmagic";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+import "swiper/dist/css/swiper.css";
 export default {
-  name: 'Section3',
+  name: "Section3",
   components: {
     swiper,
     swiperSlide
   },
-  data () {
+  data() {
     return {
       swiperOption: {
         autoplay: {
           delay: 7000
         },
-        slidesPerView:'auto',
+        slidesPerView: "auto",
         pagination: {
-          el: '.swiper-pagination'
+          el: ".swiper-pagination"
         },
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev'
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
         }
       },
-      swiperSlides: [1, 2, 3, 4, 5],
-    }
+      swiperSlides: [1]
+    };
   },
   mounted() {
     $(function() {
       // init scrollmagic
       var controller = new ScrollMagic.Controller();
 
-        // build scene with loop
-        $('.fade2').each(function() {
-          var categoryScene = new ScrollMagic.Scene({
-            triggerElement: this,
-            triggerHook: 1,
-            reverse: false
-          })
-          .setClassToggle(this, 'fade-in')
-          .addTo(controller);
+      // build scene with loop
+      $(".fade2").each(function() {
+        var categoryScene = new ScrollMagic.Scene({
+          triggerElement: this,
+          triggerHook: 1,
+          reverse: false
         })
-    })
+          .setClassToggle(this, "fade-in")
+          .addTo(controller);
+      });
+    });
   }
-}
+};
 </script>
 
 <style>
